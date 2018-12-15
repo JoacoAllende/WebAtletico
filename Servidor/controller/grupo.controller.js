6c4c55fc7ec1dcea66781050773b5ca8839b8247
JoaquinAllende <joacoallendeids@gmail.com>
2018-12-15 14:39:51 -0300

    se muestran todas las posiciones y los resultados de los grupos por separado

--- Servidor/controller/grupo.controller.js
+++ Servidor/controller/grupo.controller.js
@@ -20,7 +20,7 @@ grupoController.getGrupos = (req, res, next) => {
                             posiciones.push(element);
                         });
                         grupo.push(posiciones);
-                        const query = 'SELECT * FROM juega WHERE id_grupo = ' + i;
+                        const query = 'SELECT j.*, e1.nombre AS equipoUno, e2.nombre AS equipoDos FROM equipo e1 INNER JOIN juega j ON (j.id_equipoUno = e1.id) INNER JOIN equipo e2 ON j.id_equipoDos = e2.id WHERE j.id_grupo = ' + i + ' ORDER BY j.id_partido';
                         let partidos = [];
                         mysqlConnection.query(query, (err, rows, fields) => {
                             if (!err) {
