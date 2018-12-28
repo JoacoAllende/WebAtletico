7784384c15eca97688df8a12c6727bb562e5aa0c
JoaquinAllende <joacoallendeids@gmail.com>
2018-12-27 21:04:42 -0300

    actualice la base de datos para unificar todas en un misma y empecé a cargar los datos del nacional 2018 (todavía no estoy mostrando los datos desde esta)

--- db/BaseDeDatos.sql
+++ db/BaseDeDatos.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE IF NOT EXISTS TorneoNacional2019;
+CREATE DATABASE IF NOT EXISTS TorneoCopaCiudadDeAyacucho;
 
-USE TorneoNacional2019;
+USE TorneoCopaCiudadDeAyacucho;
 
 CREATE TABLE IF NOT EXISTS equipo (
 	id INT(2) NOT NULL AUTO_INCREMENT,
-	nombre VARCHAR(50) NOT NULL UNIQUE,
+	nombre VARCHAR(50) NOT NULL,
 	puntos INT(1) NOT NULL DEFAULT '0',
 	partidosJugados INT(1) NOT NULL DEFAULT '0',
 	partidosGanados INT(1) NOT NULL DEFAULT '0',
@@ -13,10 +13,12 @@ CREATE TABLE IF NOT EXISTS equipo (
 	golesAFavor INT(2) NOT NULL DEFAULT '0',
 	golesEnContra INT(2) NOT NULL DEFAULT '0',
 	diferenciaGoles INT(2) NOT NULL DEFAULT '0',
+	golesEnContraTotal INT(2) NOT NULL DEFAULT '0',
 	grupo INT(1) NOT NULL,
-	posicion INT(1) NULL,
 	cantAmarillas INT(2) NOT NULL DEFAULT '0',
 	cantRojas INT(2) NOT NULL DEFAULT '0',
+	año INT(4) NULL,
+	torneo BOOLEAN NULL, -- 0 es para el torneo nacional
 	PRIMARY KEY (id));
 
 CREATE TABLE IF NOT EXISTS goleadores (
@@ -26,6 +28,8 @@ CREATE TABLE IF NOT EXISTS goleadores (
 	numero INT(2) NOT NULL,
 	goles INT(2) NOT NULL,
 	id_equipo INt(2) NOT NULL,
+	año INT(4) NULL,
+	torneo BOOLEAN NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (id_equipo) REFERENCES equipo(id));
 
@@ -39,6 +43,9 @@ CREATE TABLE IF NOT EXISTS juega (
 	penalesVisitante INT(2) NULL DEFAULT '-1',
 	id_grupo INT(1) NULL,
 	instancia VARCHAR(3) NULL,
+	año INT(4) NULL,
+	torneo BOOLEAN NULL,
+	dia DATETIME NULL,
 	PRIMARY KEY (id_partido),
 	FOREIGN KEY (id_equipoUno) REFERENCES equipo(id),
 	FOREIGN KEY (id_equipoDos) REFERENCES equipo(id));
