0276a94f6ffcd8e8226a0d1f24e9377e30f29029
JoaquinAllende <joacoallendeids@gmail.com>
2018-12-30 10:41:20 -0300

    se muestran los distintos componentes apretando los distintos botones

--- Vista/src/app/app.component.ts
+++ Vista/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from './services/global.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private globalService : GlobalService){}
+
+  private cambiarRuta(ruta){
+    this.globalService.ruta = ruta;
+  }
+
   title = 'Vista';
 }
