b66442fc1d8fb2904eeca6b3dff088b2a874399b
dsjsanttt <santycosta_sk8@hotmai.com>
2019-01-15 14:41:03 -0300

    Se agrego el modulo promisify. permite promesas ; async await

--- Servidor/database.js
+++ Servidor/database.js
@@ -1,5 +1,32 @@
 const mysql = require('mysql');
 
+const {promisify} = require("util");
+
+const { database } = require('./keys');
+
+
+const mysqlConnection = mysql.createPool(database);
+
+mysqlConnection.getConnection((err, connection) => {
+    if (err) {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.');
+      }
+      if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has to many connections');
+      }
+      if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused');
+      }
+    }
+  
+    if (connection) connection.release();
+    console.log('DB is Connected');
+  
+    return;
+  });
+
+/*
 const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -14,6 +41,8 @@ mysqlConnection.connect(function(err){
     } else {
         console.log('db is connected');
     }
-});
+}); */
+
+mysqlConnection.query =  promisify(mysqlConnection.query);
 
 module.exports = mysqlConnection;
