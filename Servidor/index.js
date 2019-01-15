ee96439f4d53fe901b14b12cc4f14b390c0a596f
dsjsanttt <santycosta_sk8@hotmai.com>
2019-01-15 14:42:22 -0300

    Se agregaron nuevos modulos. Funciona

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,17 +1,46 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const morgan = require("morgan");
+const flash = require("connect-flash");
+const session = require("express-session");
+const validator = require("express-validator");
+const MySQLStore = require('express-mysql-session')(session);
 
-require('./database')
+//const mySqlConnection = require('./database');
+const { database } = require('./keys');
+
+const passport = require('passport');
+require("./lib/passport");
 
 //Settings
 
 app.set('port', process.env.PORT || 3000);
 
 //Middlewares
-
+app.use(session({
+    secret: "atleticomysqlsession",
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(validator());
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:4200'}));
+app.use(cors({ origin: 'http://localhost:4200' }));
+
+
+//Global variables
+
+app.use((req, res, next) => {
+    app.locals.success = req.flash("success");
+    app.locals.message = req.flash("message");
+    app.locals.user = req.user;
+    next();
+});
 
 //Routes
 
@@ -20,6 +49,8 @@ app.use(require('./routes/grupo.routes'));
 app.use(require('./routes/copaOro.routes'));
 app.use(require('./routes/goleadores.routes'));
 app.use(require('./routes/copaPlata.routes'));
+app.use(require('./routes/autenticacion.routes'));
+
 
 //Starting the Server
 
