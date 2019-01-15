a274eeb5aa0141ff6f50480f124c598b320b8f5c
dsjsanttt <santycosta_sk8@hotmai.com>
2019-01-15 14:46:36 -0300

    Se agrego la ruta login y signup

--- Vista/src/app/app-routing.module.ts
+++ Vista/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { CopaOroComponent } from './components/copa-oro/copa-oro.component';
 import { CopaPlataComponent } from './components/copa-plata/copa-plata.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ActualizarGoleadoresComponent } from './components/actualizar-goleadores/actualizar-goleadores.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { FormSignUpComponent } from './components/form-sign-up/form-sign-up.component';
 
 const routes: Routes = [
   
@@ -15,6 +17,8 @@ const routes: Routes = [
   { path: 'copaPlata/:torneo/:año', component: CopaPlataComponent },
   { path: 'goleadores/:torneo/:año', component: GoleadoresComponent },
   { path: 'goleadores/actualizar', component: ActualizarGoleadoresComponent },
+  { path: 'login', component: UsuarioComponent},
+  { path: 'signup', component: FormSignUpComponent},
   { path: '**', redirectTo: 'inicio'}
 ];
 
