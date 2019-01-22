cff739bd1a0b14fd0e0c28ffcdeb9b9210f23802
JoaquinAllende <joacoallendeids@gmail.com>
2019-01-22 19:21:47 -0300

    Elimina componente dedicado a la actualizacion de goleadores

--- Vista/src/app/app-routing.module.ts
+++ Vista/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { GoleadoresComponent } from './components/goleadores/goleadores.componen
 import { CopaOroComponent } from './components/copa-oro/copa-oro.component';
 import { CopaPlataComponent } from './components/copa-plata/copa-plata.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { ActualizarGoleadoresComponent } from './components/actualizar-goleadores/actualizar-goleadores.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { FormSignUpComponent } from './components/form-sign-up/form-sign-up.component';
 import { JugadoresComponent } from './components/jugadores/jugadores.component';
--- Vista/src/app/app.module.ts
+++ Vista/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { CopaOroComponent } from './components/copa-oro/copa-oro.component';
 import { GoleadoresComponent } from './components/goleadores/goleadores.component';
 import { CopaPlataComponent } from './components/copa-plata/copa-plata.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { ActualizarGoleadoresComponent } from './components/actualizar-goleadores/actualizar-goleadores.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { FormSignUpComponent } from './components/form-sign-up/form-sign-up.component';
 import { JugadoresComponent } from './components/jugadores/jugadores.component';
@@ -27,7 +26,6 @@ import { JugadoresComponent } from './components/jugadores/jugadores.component';
     GoleadoresComponent,
     CopaPlataComponent,
     InicioComponent,
-    ActualizarGoleadoresComponent,
     UsuarioComponent,
     FormSignUpComponent,
     JugadoresComponent,
