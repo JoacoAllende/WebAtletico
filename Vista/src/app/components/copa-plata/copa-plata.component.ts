551f0d5ad8b25c43dc4c147237bfb02abb23ef52
Joaquin Allende <joacoallendeids@gmail.com>
2019-02-09 10:20:30 -0300

    La vista permite modificar los partidos de la copa de plata desde la interfaz

--- Vista/src/app/components/copa-plata/copa-plata.component.ts
+++ Vista/src/app/components/copa-plata/copa-plata.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CopaPlataService } from '../../services/copa-plata.service'
 import { Instancia } from 'src/app/models/instancia';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Partido } from 'src/app/models/partido';
+import { NgForm } from '@angular/forms';
+import { GlobalsService } from 'src/app/services/globals.service';
 
 @Component({
   selector: 'app-copa-plata',
@@ -10,7 +13,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class CopaPlataComponent implements OnInit {
 
-  constructor(public copaPlataService : CopaPlataService, private rutaActiva : ActivatedRoute) { }
+  constructor(public copaPlataService : CopaPlataService, private rutaActiva : ActivatedRoute, public globals :GlobalsService) { }
 
   ngOnInit() {
     this.rutaActiva.params.subscribe(
@@ -27,6 +30,25 @@ export class CopaPlataComponent implements OnInit {
       .subscribe(res => {
         this.copaPlataService.instancias = res as Instancia[];
       })
+  }
+
+  editForm(partido: Partido) {
+    this.copaPlataService.selectedPartido = partido;
+  }
+
+  resetForm(form?: NgForm){
+    if(form){
+      form.reset();
+      this.copaPlataService.selectedPartido = new Partido();
+    }
+  }
+
+  editPartido(torneo, año, form : NgForm){
+    this.copaPlataService.putPartido(torneo, año, form.value)
+    .subscribe(res => {
+      this.resetForm(form);
+      this.getInstancias(torneo, año);
+    })
   }
 
 }
