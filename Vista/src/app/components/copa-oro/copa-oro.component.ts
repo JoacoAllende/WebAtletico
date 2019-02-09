7ab23da90c7ff3e4d21af813480a9a6601404fb6
Joaquin Allende <joacoallendeids@gmail.com>
2019-02-09 10:08:01 -0300

    La vista permite editar los partidos de la copa de oro desde la interfaz

--- Vista/src/app/components/copa-oro/copa-oro.component.ts
+++ Vista/src/app/components/copa-oro/copa-oro.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CopaOroService } from 'src/app/services/copa-oro.service';
 import { Instancia } from 'src/app/models/instancia';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Partido } from 'src/app/models/partido';
+import { NgForm } from '@angular/forms';
+import { GlobalsService } from 'src/app/services/globals.service';
 
 @Component({
   selector: 'app-copa-oro',
@@ -11,7 +14,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class CopaOroComponent implements OnInit {
 
-  constructor(public copaOroService : CopaOroService, private rutaActiva : ActivatedRoute) { }
+  constructor(public copaOroService : CopaOroService, private rutaActiva : ActivatedRoute, public globals :GlobalsService) { }
 
   ngOnInit() {
     this.rutaActiva.params.subscribe(
@@ -28,6 +31,25 @@ export class CopaOroComponent implements OnInit {
       .subscribe(res => {
         this.copaOroService.instancias = res as Instancia[];
       })
+  }
+
+  editForm(partido: Partido) {
+    this.copaOroService.selectedPartido = partido;
+  }
+
+  resetForm(form?: NgForm){
+    if(form){
+      form.reset();
+      this.copaOroService.selectedPartido = new Partido();
+    }
+  }
+
+  editPartido(torneo, año, form : NgForm){
+    this.copaOroService.putPartido(torneo, año, form.value)
+    .subscribe(res => {
+      this.resetForm(form);
+      this.getInstancias(torneo, año);
+    })
   }
 
 }
--- Vista/src/app/services/copa-oro.service.ts
+++ Vista/src/app/services/copa-oro.service.ts
@@ -2,21 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Instancia } from '../models/instancia';
 import { GlobalsService } from '../services/globals.service';
+import { Partido } from '../models/partido';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CopaOroService {
 
+  selectedPartido : Partido;
   instancias : Instancia[];
   API_URI;
 
   constructor(private http : HttpClient, private globalsService : GlobalsService) {
     this.instancias = [];
+    this.selectedPartido = new Partido;
     this.API_URI = globalsService.API_URI;
    }
 
   getInstancias(to, a){
     return this.http.get(`http://${this.API_URI}/copaOro/${to}/${a}`);
   }
+
+  putPartido(to, a, partido: Partido){
+    return this.http.put(`http://${this.API_URI}/copaOro/${to}/${a}`,partido);
+   }
+
 }
