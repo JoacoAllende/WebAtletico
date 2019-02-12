6235e50f9bbc04af34e31b3efb2a9ba81cbbda29
Joaquin Allende <joacoallendeids@gmail.com>
2019-02-12 07:54:41 -0300

    Arregla bug del servidor

--- Servidor/controller/copaPlata.contoller.js
+++ Servidor/controller/copaPlata.contoller.js
@@ -4,7 +4,7 @@ const mysqlConnection = require('../database');
 
 copaPlataController.getPartidos = (req, res, next) => {
     let instancias = [];
-    const query = 'SELECT j.*, date(dia) AS fecha, hour(dia) AS hora, minute(dia) as minutos, e1.nombre AS equipoUno, e2.nombre AS equipoDos FROM equipo e1 INNER JOIN juega j ON (j.id_equipoUno = e1.id) INNER JOIN equipo e2 ON (j.id_equipoDos = e2.id) WHERE j.instancia = "ccp" AND j.torneo = ' + req.params.to + ' AND j.anio = ' + req.params.a + ' ORDER BY j.id_partido';
+    const query = 'SELECT j.*, date(j.dia) AS fecha, hour(j.dia) AS hora, minute(j.dia) as minutos, e1.nombre AS equipoUno, e2.nombre AS equipoDos FROM equipo e1 INNER JOIN juega j ON (j.id_equipoUno = e1.id) INNER JOIN equipo e2 ON (j.id_equipoDos = e2.id) WHERE j.instancia = "ccp" AND j.torneo = ' + req.params.to + ' AND j.anio = ' + req.params.a + ' ORDER BY j.id_partido';
     mysqlConnection.query(query, (err, rows, fields) => {
         if (!err) {
             let instancia = [];
@@ -16,7 +16,7 @@ copaPlataController.getPartidos = (req, res, next) => {
             instancia.push(["Cuartos de final"]);
             instancia.push(partidos);
             instancias.push(instancia);
-            const query = 'SELECT j.*, date(dia) AS fecha, hour(dia) AS hora, minute(dia) as minutos, e1.nombre AS equipoUno, e2.nombre AS equipoDos FROM equipo e1 INNER JOIN juega j ON (j.id_equipoUno = e1.id) INNER JOIN equipo e2 ON (j.id_equipoDos = e2.id) WHERE j.instancia = "scp" AND j.torneo = ' + req.params.to + ' AND j.anio = ' + req.params.a + ' ORDER BY j.id_partido';
+            const query = 'SELECT j.*, date(j.dia) AS fecha, hour(j.dia) AS hora, minute(j.dia) as minutos, e1.nombre AS equipoUno, e2.nombre AS equipoDos FROM equipo e1 INNER JOIN juega j ON (j.id_equipoUno = e1.id) INNER JOIN equipo e2 ON (j.id_equipoDos = e2.id) WHERE j.instancia = "scp" AND j.torneo = ' + req.params.to + ' AND j.anio = ' + req.params.a + ' ORDER BY j.id_partido';
             mysqlConnection.query(query, (err, rows, fields) => {
                 if (!err) {
                     let instancia = [];
@@ -28,7 +28,7 @@ copaPlataController.getPartidos = (req, res, next) => {
                     instancia.push(["Semifinales"]);
                     instancia.push(partidos);
                     instancias.push(instancia);
-                    const query = 'SELECT j.*, date(dia) AS fecha, hour(dia) AS hora, minute(dia) as minutos, e1.nombre AS equipoUno, e2.nombre AS equipoDos FROM equipo e1 INNER JOIN juega j ON (j.id_equipoUno = e1.id) INNER JOIN equipo e2 ON (j.id_equipoDos = e2.id) WHERE j.instancia = "fcp" AND j.torneo = ' + req.params.to + ' AND j.anio = ' + req.params.a + ' ORDER BY j.id_partido';
+                    const query = 'SELECT j.*, date(j.dia) AS fecha, hour(j.dia) AS hora, minute(j.dia) as minutos, e1.nombre AS equipoUno, e2.nombre AS equipoDos FROM equipo e1 INNER JOIN juega j ON (j.id_equipoUno = e1.id) INNER JOIN equipo e2 ON (j.id_equipoDos = e2.id) WHERE j.instancia = "fcp" AND j.torneo = ' + req.params.to + ' AND j.anio = ' + req.params.a + ' ORDER BY j.id_partido';
                     mysqlConnection.query(query, (err, rows, fields) => {
                     if (!err) {
                         let instancia = [];
@@ -57,7 +57,7 @@ copaPlataController.getPartidos = (req, res, next) => {
 };
 
 copaPlataController.getPartido = (req, res, next) => {
-    const query = 'SELECT *, date(dia) AS fecha, hour(dia) AS hora, minute(dia) as minutos, FROM juega WHERE id_partido = ' + req.params.id;
+    const query = 'SELECT *, date(j.dia) AS fecha, hour(j.dia) AS hora, minute(j.dia) as minutos, FROM juega WHERE id_partido = ' + req.params.id;
     mysqlConnection.query(query, (err, rows, fields) =>{
         res.json(rows);
     })
